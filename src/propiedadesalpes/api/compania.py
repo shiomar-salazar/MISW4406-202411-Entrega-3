c113a9c3130fb09703c3dd08e3b17dbcd12d650f
CommitUser-at-407747150537 <android619@gmail.com>
2024-02-26 23:54:23 -0500

    Api compania

--- src/propiedadesalpes/api/compania.py
+++ src/propiedadesalpes/api/compania.py
@@ -1,53 +1,53 @@
-import propiedadesalpes.seedwork.presentacion.api as api
-from propiedadesalpes.modulos.companias.aplicacion.mapeadores import MapeadorCompaniaDTOJson
-from propiedadesalpes.modulos.companias.aplicacion.queries.obtener_companias_registradas import ObtenerCompaniasRegistradas
-from propiedadesalpes.modulos.companias.aplicacion.queries.obtener_companias_procesadas import ObtenerCompaniasProcesadas
-from propiedadesalpes.seedwork.aplicacion.queries import ejecutar_query
-from propiedadesalpes.seedwork.aplicacion.comandos import ejecutar_commando
-from propiedadesalpes.seedwork.dominio.excepciones import ExcepcionDominio
-from propiedadesalpes.modulos.companias.aplicacion.comandos.registrar_compania import RegistrarCompania
+import seedwork.presentacion.api as api
+from seedwork.dominio.excepciones import ExcepcionDominio
+from modulos.propiedades.aplicacion.mapeadores import MapeadorCompaniaDTOJson
+from modulos.propiedades.aplicacion.comandos.crear_compania import CrearCompania
+from modulos.propiedades.aplicacion.queries.obtener_todas_companias import ObtenerTodasCompanias
+from seedwork.aplicacion.queries import ejecutar_query
+from seedwork.aplicacion.comandos import ejecutar_commando
 import json
-from flask import request
 from flask import Response
+from flask import redirect, render_template, request, session, url_for
 
 
-bp = api.crear_blueprint('companias', '/companias')
+bp = api.crear_blueprint('compania', '/compania')
 
-@bp.route('/compania-query/registradas', methods=('GET',))
-def dar_companias_registradas():
-    print('<================ API.dar_companias_registradas ================>')
-    query_resultado = ejecutar_query(ObtenerCompaniasRegistradas())
-    print(query_resultado)
-    map_compania = MapeadorCompaniaDTOJson()
-    print('<================ API.dar_companias_registradas ================>')
-    return map_compania.dto_a_externo(query_resultado.resultado)
-
-@bp.route('/compania-query/procesadas', methods=('GET',))
-def dar_companias_procesadas():
-    print('<================ API.dar_companias_procesadas ================>')
-    query_resultado = ejecutar_query(ObtenerCompaniasProcesadas())
-    print(query_resultado)
-    map_compania = MapeadorCompaniaDTOJson()
-    print('<================ API.dar_companias_procesadas ================>')
-    return map_compania.dto_a_externo(query_resultado.resultado)
-
-@bp.route('/compania-comando', methods=('POST',))
-def registrar_asincrona():
+@bp.route('/crear', methods=['POST',])
+def crear():
     try:
         compania_dict = request.json
-
         map_compania = MapeadorCompaniaDTOJson()
         compania_dto = map_compania.externo_a_dto(compania_dict)
 
-        comando = RegistrarCompania(compania_dto.id, compania_dto.nombre_compania, compania_dto.representante_legal,
-                                    compania_dto.email_contacto, compania_dto.telefono_contacto, compania_dto.estado)
-                                    # compania_dto.documento_identidad, compania_dto.tipo_industria, compania_dto.localizacion)
-        
-        # TODO Reemplaze es todo código sincrono y use el broker de eventos para propagar este comando de forma asíncrona
-        # Revise la clase Despachador de la capa de infraestructura
+        comando = CrearCompania(
+           nombre_compania = compania_dto.nombre_compania,
+            representante_legal = compania_dto.representante_legal,
+            email_contacto = compania_dto.email_contacto,
+            telefono_contacto = compania_dto.telefono_contacto,
+            estado = compania_dto.estado,
+            documento_identidad_numero_identificacion = compania_dto.documento_identidad_numero_identificacion,
+            documento_identidad_tipo = compania_dto.documento_identidad_tipo,
+            tipo_industria = compania_dto.tipo_industria,
+            direccion = compania_dto.direccion,
+            latitud = compania_dto.tipo_industria,
+            longitud = compania_dto.tipo_industria,
+            ciudad = compania_dto.ciudad,
+            pais = compania_dto.pais
+        )
 
         ejecutar_commando(comando)
-        
         return Response('{}', status=202, mimetype='application/json')
     except ExcepcionDominio as e:
         return Response(json.dumps(dict(error=str(e))), status=400, mimetype='application/json')
+    
+@bp.route('', methods=('GET',))
+def dar_reserva_usando_query():
+    map_compania = MapeadorCompaniaDTOJson()
+    query_resultado = ejecutar_query(ObtenerTodasCompanias())
+    resultados = []
+    
+    for compania in query_resultado.resultado:
+        resultados.append(map_compania.dto_a_externo(compania))
+    
+    return resultados
+    
