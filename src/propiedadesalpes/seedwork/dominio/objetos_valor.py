73453b407cb8f37af4b4ee37695c0213fc79e146
Jorge M. Carrillo <jm.carrillo@uniandes.edu.co>
2024-02-26 22:51:03 -0600

    seedwork dominio ov

--- src/propiedadesalpes/seedwork/dominio/objetos_valor.py
+++ src/propiedadesalpes/seedwork/dominio/objetos_valor.py
@@ -1,48 +1,52 @@
-"""Objetos valor reusables parte del seedwork del proyecto
-
-En este archivo usted encontrará los objetos valor reusables parte del seedwork del proyecto
-
-"""
-
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from .entidades import Locacion
 from datetime import datetime
 
-@dataclass(frozen=True)
+#@dataclass(frozen=True)
 class ObjetoValor:
     ...
 
-@dataclass(frozen=True)
-class Codigo(ABC, ObjetoValor):
-    codigo: str
+#@dataclass(frozen=True)
+class InformacionGeoespacial(ABC, ObjetoValor):
+    latitud: str
+    longitud: str
 
-class Ruta(ABC, ObjetoValor):
-    @abstractmethod
-    def origen(self) -> Locacion:
-        ...
-    
-    @abstractmethod
-    def destino(self) -> Locacion:
-        ...
-    
-    @abstractmethod
-    def fecha_salida(self) -> datetime:
-        ...
+#@dataclass(frozen=True)
+class DatosGreograficos(ABC, ObjetoValor):
+    ciudad: str
+    pais: str
 
-    @abstractmethod
-    def fecha_llegada(self) -> datetime:
-        ...
+#@dataclass(frozen=True)
+class Direccion(ABC, ObjetoValor):
+    direccion: str
+    datos_geograficos: DatosGreograficos
 
-@dataclass(frozen=True)
-class Pais(ObjetoValor):
-    codigo: Codigo
+#@dataclass(frozen=True)
+class Localizacion(ABC, ObjetoValor):
+    direccion: Direccion
+    infromacion_geoespacial: InformacionGeoespacial
+
+#@dataclass(frozen=True)
+class TipoIndustria(ABC, ObjetoValor):
+    id_industria: str
     nombre: str
+    descripcion: str
 
-@dataclass(frozen=True)
-class Ciudad(ObjetoValor):
-    pais: Pais
-    codigo: Codigo
-    nombre: str
-
-
+#@dataclass(frozen=True)
+class DocumentoIdentidad(ABC, ObjetoValor):
+    tipo: str
+    numero_identificacion: str
+    
+    
+#@dataclass(frozen=True)
+class Compania_ov(ABC, ObjetoValor):
+    id_comp: str
+    nombre_compania: str
+    representante_legal: str
+    email_contacto: str
+    telefono_contacto: str
+    estado: str
+    documento_identidad: DocumentoIdentidad
+    tipo_industria: TipoIndustria
+    localizacion: Localizacion
